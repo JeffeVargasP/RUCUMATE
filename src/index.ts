ea858a0917da5d0ce2eeb072d62656740f46f273
JeffeVargasP <jeffersonvargas745@gmail.com>
2024-05-16 17:56:30 -0300

    chore: Update CORS configuration to allow additional HTTP methods

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,11 @@ const app: Express = express();
 const { serverPort } = config;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept'
+}));
 app.options('*', cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
